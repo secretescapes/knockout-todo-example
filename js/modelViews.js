f5d9687fe93e803221cf8079798b652df5cc4f0e
Mario <mario.martinez@secretescapes.com>
2016-11-02 15:36:53 +0000

    Check tasks

--- js/modelViews.js
+++ js/modelViews.js
@@ -1,10 +1,18 @@
 function TasksViewModel() {
-    this.tasks = ko.observableArray([{name: 'Make to-do app', assignee: 'Mario'}]);
+    this.tasks = ko.observableArray([{
+    	name: 'Make to-do app',
+    	assignee: 'Mario',
+    	done: ko.observable(true)
+    }]);
     this.newTaskName = ko.observable();
     this.newTaskAssignee = ko.observable('Mario');
 
     this.addNewTask = function() {
-    	this.tasks.push({name: this.newTaskName(), assignee: this.newTaskAssignee()});
+    	this.tasks.push({
+    		name: this.newTaskName(),
+    		assignee: this.newTaskAssignee(),
+    		done: ko.observable(false)
+    	});
     	this.newTaskName('');
     	this.newTaskAssignee('Mario');
     }
