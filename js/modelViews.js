76e46a656f28b145b77e9311a08f1c12ced1026a
Mario <mario.martinez@secretescapes.com>
2016-11-02 15:11:44 +0000

    Functionality to add tasks

--- js/modelViews.js
+++ js/modelViews.js
@@ -1,4 +1,12 @@
 function TasksViewModel() {
     this.tasks = ko.observableArray([{name: 'Make to-do app', assignee: 'Mario'}]);
+    this.newTaskName = ko.observable();
+    this.newTaskAssignee = ko.observable('Mario');
+
+    this.addNewTask = function() {
+    	this.tasks.push({name: this.newTaskName(), assignee: this.newTaskAssignee()});
+    	this.newTaskName('');
+    	this.newTaskAssignee('Mario');
+    }
 }
 
